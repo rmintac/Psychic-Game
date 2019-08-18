a53465b93dba0c393f06db6aed8c0e0f65ce92ea
Rebekah Mintac <rmintac@gmail.com>
2019-08-17 23:46:54 -0400

    a

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,38 @@
-var computerChoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-
-var userGuess = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-var h1 = document.getElementById('test')
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var guessesSoFar = [];
+var winsPlayer = document.getElementById("wins");
+var lossesPlayer = document.getElementById("losses");
+var guessesLeftPlayer = document.getElementById("guesses-left");
+var guessesRemaining = document.getElementById("guesses-so-far");
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+console.log(computerGuess);
 document.onkeyup = function (event) {
-
-    console.log('Player Guess ', event.key);
-
-    var computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
-    console.log(computerGuess);
+ var userGuess = event.key;
+ // console.log(userGuess);
+ guessesLeft--;
+ guessesSoFar.push(userGuess);
+ console.log(guessesLeft);
+ if (userGuess === computerGuess) {
+   wins++;
+   guessesLeft = 9;
+   guessesSoFar = [];
+   computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+   console.log(computerGuess);
+ } else if (guessesLeft === 0) {
+   alert("Game Over");
+   guessesLeft = 9;
+   guessesSoFar= [];
+   console.log(guessesSoFar);
+   losses++;
+   computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+   console.log(computerGuess);
+ }
+ winsPlayer.innerHTML = "Wins: " + wins;
+ lossesPlayer.innerHTML = "Losses: " + losses;
+ guessesLeftPlayer.innerHTML = "Guesses Left: " + guessesLeft;
+ guessesRemaining.innerHTML = "Your Guesses So Far: " + guessesSoFar;
 }
